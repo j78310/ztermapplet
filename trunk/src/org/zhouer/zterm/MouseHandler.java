91ba8d4a6e302584ce58cadca1dd67e1b5fdcfeb
Chin.Chang.Yang <Chin.Chang.Yang@8e8e4a4b-7554-0410-b156-217a0b45ef96>
2008-08-26 14:41:24 +0000

    Update version to 2.0 BETA

---
+++ trunk/src/org/zhouer/zterm/MouseHandler.java
@@ -0,0 +1,56 @@
+package org.zhouer.zterm;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.SwingUtilities;
+
+public class MouseHandler implements MouseListener {
+	
+	private Model model;
+	
+	private ZTerm view;
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
+
+	public void setView(ZTerm view) {
+		this.view = view;
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		// Clicked right button
+		if (e.getButton() == MouseEvent.BUTTON3) {
+			final int x = e.getX();
+			final int y = e.getY();
+			
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					view.popupMenu.show(view, x, y);
+				}
+			});
+		}
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
