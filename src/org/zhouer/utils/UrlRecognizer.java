a2a9ad2d6b098bb8dc0c9ca653fd67eefc7d8d31
Chin.Chang.Yang <Chin.Chang.Yang@8e8e4a4b-7554-0410-b156-217a0b45ef96>
2009-01-04 12:07:29 +0000

    Merge revision 95 to head.

--- src/org/zhouer/utils/UrlRecognizer.java
+++ src/org/zhouer/utils/UrlRecognizer.java
@@ -13,18 +13,13 @@ public class UrlRecognizer {
 	}
 	
 	/**
-	 * Detect a position of a message is a part of HTTP.
-	 * 
-	 * For example, message = "Books, and Flowers: http://123456789", index = 5,
-	 * it should return false.
-	 * message = "Books, and Flowers: http://123456789", index = 30,
-	 * it should return true.
-	 * 
-	 * @param message message that contains HTTP
-	 * @param index index of message to be detected whether it is part of HTTP
-	 * @return true, if the character corresponding to the index in the massage is a part of HTTP; false, otherwise. 
+	 * Detect a position of a message is a part of the protocol.
+	 * @param protocol the protocol to check
+	 * @param message message that contains the protocol
+	 * @param index index of message to be detected whether it is part of the protocol
+	 * @return true, if the character corresponding to the index in the massage is a part of the protocol; false, otherwise. 
 	 */
-	public static boolean isPartOfHttp(final String message, final int index) {
+	public static boolean partialMatch(final String protocol, final String message, final int index) {
 		// 1. message: null 2. message: "words" 3. message: "http://test http://test" 4. message: "http://test/測試"  5. message: "http://test" 
 		if (message == null) {
 			return false;
@@ -34,7 +29,7 @@ public class UrlRecognizer {
 			throw new IllegalArgumentException("Out of bound!");
 		}
 		
-		final int lastIndexOfHttp = message.lastIndexOf("http://", index);
+		final int lastIndexOfHttp = message.lastIndexOf(protocol + "://", index);
 		final boolean httpNotFound = lastIndexOfHttp == -1;
 
 		// 1. message: "words" 2. message: "http://test http://test" 3. message: "http://test/測試"  4. message: "http://test" 
@@ -69,5 +64,42 @@ public class UrlRecognizer {
 		
 		// 1. message: "http://test http://test"
 		return false;
+	}
+	
+	/**
+	 * Detect a position of a message is a part of HTTP.
+	 * 
+	 * For example, message = "Books, and Flowers: http://123456789", index = 5,
+	 * it should return false.
+	 * message = "Books, and Flowers: http://123456789", index = 30,
+	 * it should return true.
+	 * 
+	 * @param message message that contains HTTP
+	 * @param index index of message to be detected whether it is part of HTTP
+	 * @return true, if the character corresponding to the index in the massage is a part of HTTP; false, otherwise. 
+	 */
+	public static boolean isPartOfHttp(final String message, final int index) {
+		return partialMatch("http", message, index);
+	}
+	
+	/**
+	 * Detect a position of a message is a part of FTP.
+	 * @param message message that contains FTP
+	 * @param index index of message to be detected whether it is part of FTP
+	 * @return true, if the character corresponding to the index in the massage is a part of FTP; false, otherwise. 
+	 */
+	public static boolean isPartOfFtp(final String message, final int index) {
+		return partialMatch("ftp", message, index);
+	}
+	
+	/**
+	 * Detect a position of a message is a part of HTTPS.
+	 * 
+	 * @param message message that contains HTTPS
+	 * @param index index of message to be detected whether it is part of HTTPS
+	 * @return true, if the character corresponding to the index in the massage is a part of HTTPS; false, otherwise. 
+	 */
+	public static boolean isPartOfHttps(final String message, final int index) {
+		return partialMatch("https", message, index);
 	}
 }
